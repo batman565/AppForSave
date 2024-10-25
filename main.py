8d53c1c83b90a62a2464bd531ff8b3c8a5a9e7d4
mihail drem <mixail_dremov@mail.ru>
2024-10-25 19:08:24 +0300

    the main.py has been changed

--- main.py
+++ main.py
@@ -5,27 +5,44 @@ from tqdm import tqdm
 import time
 
 
-token = ''
+token_vk = 'Ваш токен ВК'
 
 
 class VK:
-    def __init__(self, access_token, user_id, version='5.199'):
+    def __init__(self, access_token, version='5.199'):
         self.token = access_token
-        self.id = user_id
         self.version = version
         self.params = {'access_token': self.token, 'v': self.version}
 
-    def users_info(self):
-        url = 'https://api.vk.com/method/users.get'
-        params = {'user_ids': self.id}
+    def get_photos(self, ids):
+        url = 'https://api.vk.com/method/photos.get'
+        params = {'owner_id': ids, 'album_id': 'profile', 'extended': '1'}
         response = requests.get(url, params={**self.params, **params})
         return response.json()
 
-    def get_photos(self):
-        url = 'https://api.vk.com/method/photos.get'
-        params = {'owner_id': self.id, 'album_id': 'profile', 'extended': '1'}
-        response = requests.get(url, params={**self.params, **params})
-        return response.json()
+    def parse_vk_photos(self, user_id, count_user):
+        photos_data = []
+        count = int(vk.get_photos(user_id)['response']['count'])
+
+        if count_user <= count:
+            for i in range(count_user):
+                photo_info = vk.get_photos(user_id)['response']['items'][i]
+                photo_url = photo_info['orig_photo']['url']
+                likes = photo_info['likes']['count']
+                size_h = photo_info['orig_photo']['height']
+                size_w = photo_info['orig_photo']['width']
+                filename = f'{likes}.jpg'
+                pathfile = f'savedVK/{filename}'
+                photos_data.append({
+                    'url': photo_url,
+                    'likes': likes,
+                    'size_h': size_h,
+                    'size_w': size_w,
+                    'filename': filename,
+                    'pathfile': pathfile
+                })
+
+        return photos_data
 
 
 class Yandex:
@@ -42,10 +59,10 @@ class Yandex:
         }
         return requests.put(url_create, params=params, headers=headers)
 
-    def upload_photos(self, filename, path):
+    def upload_photos(self, fname, path):
         url_upload = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
         params = {
-            'path': f'{path}/{filename}',
+            'path': f'{path}/{fname}',
             'overwrite': 'true'
         }
         headers = {
@@ -53,38 +70,31 @@ class Yandex:
         }
         response = requests.get(url_upload, params=params, headers=headers)
         upload_link = response.json()['href']
-        with open(pathfile, 'rb') as f:
+        with open(photo["pathfile"], 'rb') as f:
             return requests.put(upload_link, files={'file': f})
 
 
 acc_token = input("Введите ваш Токен Яндекс диска: ")
-user_id = input("Введите ваш id в ВК: ")
+user_id = str(input("Введите ваш id в ВК: "))
 path = input("Введите путь в формате (/path/...): ")
-access_token = token
-vk = VK(access_token, user_id)
-count = int(vk.get_photos()['response']['count'])
+vk = VK(token_vk)
 data = {'photo': []}
 count_user = int(input("Введите количество фотографий для сохранения: "))
 
-if count_user <= count:
-    for i in range(0, count_user):
-        photo = (vk.get_photos()['response']['items'][i]['orig_photo']['url'])
-        likes = (vk.get_photos()['response']['items'][i]['likes']['count'])
-        size_h = (vk.get_photos()['response']['items'][i]['orig_photo']['height'])
-        size_w = (vk.get_photos()['response']['items'][i]['orig_photo']['width'])
-        filename = f'{likes}.jpg'
-        pathfile = f'savedVK/{filename}'
+photos_data = vk.parse_vk_photos(user_id, count_user)
 
-        img_data = requests.get(photo).content
-        with open(pathfile, 'wb') as handler:
+if len(photos_data) > 0:
+    for photo in photos_data:
+        img_data = requests.get(photo['url']).content
+        with open(photo["pathfile"], 'wb') as handler:
             handler.write(img_data)
 
         yandex = Yandex(acc_token)
         yandex.create_folder(path)
-        yandex.upload_photos(filename, path)
+        yandex.upload_photos(photo["filename"], path)
 
-        data['photo'].append({'filename': filename,
-                              'size': f'Height: {size_h}, width: {size_w}'})
+        data['photo'].append({'filename': photo['filename'],
+                              'size': f'Height: {photo["size_h"]}, width: {photo["size_w"]}'})
 
         with open('photo.json', 'w') as file1:
             json.dump(data, file1)
